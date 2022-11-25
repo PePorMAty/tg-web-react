3ff001825e5d43770c790aad9c2327f72f416d8f
PePorMAty <themranti3256@gmail.com>
2022-11-25 23:48:27 +0700

    fsddsf

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -22,7 +22,31 @@ const getTotalPrice = (items = []) => {
 
 export const ProductList = () => {
   const [addedItems, setAddedItems] = useState([])
-  const {tg} = useTelegram()
+  const {tg, queryId} = useTelegram()
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    }
+    /* tg.sendData(JSON.stringify(data)) */
+    fetch('http//localhost:9999', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [onSendData])
+
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find(item => item.id === product.id)
     let newItems = []
