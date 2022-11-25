615c2cb51a5a3e19ecf0ff90bd4f9e7f4cdf2188
PePorMAty <themranti3256@gmail.com>
2022-11-25 19:27:56 +0700

    fds

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -47,7 +47,7 @@ export const ProductList = () => {
 
   return (
     <div className='list'>
-      {ProductList.map(item => (
+      {products.map(item => (
         <ProductItem
           product={item}
           onAdd={onAdd}
